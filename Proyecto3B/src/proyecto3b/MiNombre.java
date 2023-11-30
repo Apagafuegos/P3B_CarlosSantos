14142041ae55ec8d0d448d1ead9055f239c119df
Apagafuegos <csantos6952@ceuandalucia.es>
2023-11-30 16:11:12 +0100

    Subo la clase que imprime mi nombre

---
+++ Proyecto3B/src/proyecto3b/MiNombre.java
@@ -0,0 +1,10 @@
+package proyecto3b;
+
+public class MiNombre {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Carlos Santos Expósito");
+	}
+
+}
